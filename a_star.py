eff5bd43d032ebee67e3bab8f52ec4155a7ad619
Peter van der Heijden <peter@vanderheijden.name>
2022-09-03 22:25:22 +0200

    Rename some function parameters.

--- a_star.py
+++ a_star.py
@@ -7,25 +7,24 @@ def search(
         start: TItem,
         end: TItem,
         get_next: Callable[[TItem], Iterator[TItem]],
-        get_distance: Callable[[TItem], int],
-        max_depth: Optional[int] = None) -> Optional[list[TItem]]:
-    """Do an A* search for `end` starting at `start`.
+        get_min_distance: Callable[[TItem], int],
+        max_length: Optional[int] = None) -> Optional[list[TItem]]:
+    """Find the shortest path from `start` to `end`.
 
     Args:
         start (TItem): The starting point for the search.
         end (TItem): The end point for the search.
         get_next (Callable[[TItem], Iterator[TItem]]): A function that returns all items
             that can be reached from the given item.
-        get_distance: (Callable[[TItem], int]): A function that returns a lower bound for
+        get_min_distance: (Callable[[TItem], int]): A function that returns a lower bound for
             the number of steps from the given item to `end`. The return value should be
             as high as possible but never higher than the actual number of steps.
-        max_depth (Optional[int], optional): The maximum number of steps from `start` to `end`.
-            If a path from `start` to `end` is known its, length should be supplied here.
-            Defaults to None.
+        max_length (Optional[int], optional): The maximum number of steps from `start` to `end`.
+            If an upper bound for the length of the path from `start` to `end` is known, it
+            should be supplied here. Defaults to None.
 
     Returns:
         Optional[list[TItem]]: A list with the steps from `start` to `end` or None.
     """
     if start == end:
         return [start]
-    get_next(start)
--- test_a_star.py
+++ test_a_star.py
@@ -1,4 +1,3 @@
-import pytest
 from a_star import search
 from typing import Iterator, TypeVar
 
@@ -6,12 +5,12 @@ TItem = TypeVar('TItem')
 
 
 def test_search_returns_start_if_equal_to_end():
-    assert search(1, 1, fail_get_next, fail_get_distance) == [1]
+    assert search(1, 1, fail_get_next, fail_get_min_distance) == [1]
 
 
 def fail_get_next(item: TItem) -> Iterator[TItem]:
     raise AssertionError(f"get_next({item}) was called.")
 
 
-def fail_get_distance(item: TItem) -> int:
-    raise AssertionError(f"get_distance({item}) was called.")
+def fail_get_min_distance(item: TItem) -> int:
+    raise AssertionError(f"get_min_distance({item}) was called.")
