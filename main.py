89068397521879bec11b081ba7d50e0f68c338e9
Peter van der Heijden <peter@vanderheijden.name>
2022-09-03 17:04:07 +0200

    Add timing for some strings.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from miu import produce
 
+# MIUIUIUI: 0:00:00.578415
+# MIUIUIUIUI: Takes forever.
+# MUIUIUIUIUIU: Takes forever.
+
 string = input("Enter string to produce: ")
 
 for step in produce(string.upper()) or []:
