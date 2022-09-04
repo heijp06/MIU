793a6883fa2476b8430a3d1cb08b7423738f2f8c
Peter van der Heijden <peter@vanderheijden.name>
2022-09-04 14:56:01 +0200

    Parameterize graph test and add a few extra.

--- test_a_star.py
+++ test_a_star.py
@@ -1,42 +1,6 @@
+import pytest
 from typing import Iterator
 from a_star import search
-
-
-def test_search_returns_start_if_equal_to_end():
-    assert search(1, 1, lambda _: [], lambda _: 0) == [1]
-
-
-def test_search_with_two_steps():
-    assert search(1, 2, lambda _: [2], lambda _: 3) == [1, 2]
-
-
-def test_search_with_cycle():
-    assert search(1, 2, lambda _: [1], lambda _: 3) is None
-
-
-def test_search_with_max_length():
-    assert search(1, 2, lambda _: [2], lambda _: 3, 2) is None
-
-
-def test_search_with_initial_wrong_path():
-    assert search(
-        1,
-        9,
-        lambda i: [3, 8] if i == 2 else [i + 1],
-        lambda i: i
-    ) == [1, 2, 8, 9]
-
-
-def test_search_graph():
-    graph = Graph({
-        "A": (1, ["B", "D"]),
-        "B": (1, ["C"]),
-        "C": (1, ["E"]),
-        "D": (2, ["E"]),
-        "E": (1, ["F"]),
-        "F": (0, [])
-    })
-    assert search("A", "F", graph.get_next, graph.get_min_distance) == ["A", "D", "E", "F"]
 
 
 class Graph:
@@ -48,3 +12,32 @@ class Graph:
 
     def get_next(self, item: str) -> Iterator[str]:
         return iter(self.graph[item][1])
+
+
+def test_search_with_cycle():
+    assert search(1, 2, lambda _: [1], lambda _: 3) is None
+
+
+def test_search_with_max_length():
+    assert search(1, 2, lambda _: [2], lambda _: 3, 2) is None
+
+
+@pytest.mark.parametrize("graph,path", (
+    (Graph({
+        "A": (0, [])
+    }), ["A"]),
+    (Graph({
+        "A": (1, ["B"]),
+        "B": (0, [])
+    }), ["A", "B"]),
+    (Graph({
+        "A": (1, ["B", "D"]),
+        "B": (1, ["C"]),
+        "C": (1, ["E"]),
+        "D": (2, ["E"]),
+        "E": (1, ["F"]),
+        "F": (0, [])
+    }), ["A", "D", "E", "F"])
+))
+def test_search_graph(graph, path):
+    assert search(path[0], path[-1], graph.get_next, graph.get_min_distance) == path
