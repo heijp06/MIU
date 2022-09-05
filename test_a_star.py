018d0ae4e941165a843a415ad1233413fb649cd8
Peter van der Heijden <peter@vanderheijden.name>
2022-09-05 21:42:52 +0200

    Automatically figure out where graphs start and end.

--- test_a_star.py
+++ test_a_star.py
@@ -6,6 +6,24 @@ from a_star import search
 class Graph:
     def __init__(self, graph: dict[str, tuple[int, list[str]]]):
         self.graph = graph
+        self.set_start_and_end()
+
+    def set_start_and_end(self):
+        starts = set(self.graph.keys())
+        ends = set(
+            node
+            for _, nodes in self.graph.values()
+            for node in nodes
+        )
+        start = starts - ends
+        assert len(start) == 1, "No single start point."
+        (self.start,) = start
+        self.end = next(
+            key
+            for key, (_, value)
+            in self.graph.items()
+            if not value
+        )
 
     def get_min_distance(self, item: str) -> int:
         return self.graph[item][0]
@@ -14,22 +32,23 @@ class Graph:
         return iter(self.graph[item][1])
 
 
-def test_search_with_cycle():
-    assert search(1, 2, lambda _: [1], lambda _: 3) is None
-
-
 def test_search_with_max_length():
     assert search(1, 2, lambda _: [2], lambda _: 3, 2) is None
 
 
-@pytest.mark.parametrize("graph,path", (
+@pytest.mark.parametrize("graph,path,max_length", (
     (Graph({
         "A": (0, [])
-    }), ["A"]),
+    }), ["A"], None),
+    (Graph({
+        "A": (1, ["B"]),
+        "B": (1, ["C"]),
+        "C": (0, [])
+    }), None, 1),
     (Graph({
         "A": (1, ["B"]),
         "B": (0, [])
-    }), ["A", "B"]),
+    }), ["A", "B"], None),
     (Graph({
         "A": (1, ["B", "D"]),
         "B": (1, ["C"]),
@@ -37,7 +56,8 @@ def test_search_with_max_length():
         "D": (2, ["E"]),
         "E": (1, ["F"]),
         "F": (0, [])
-    }), ["A", "D", "E", "F"])
+    }), ["A", "D", "E", "F"], None)
 ))
-def test_search_graph(graph, path):
-    assert search(path[0], path[-1], graph.get_next, graph.get_min_distance) == path
+def test_search_graph(graph, path, max_length):
+    assert search(graph.start, graph.end, graph.get_next,
+                  graph.get_min_distance, max_length) == path
